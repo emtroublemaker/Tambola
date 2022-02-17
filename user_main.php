19b5bad589deb1f0c78ffee9ee55c3ae9a763303
emtroublemaker <83471925+emtroublemaker@users.noreply.github.com>
2022-02-17 17:27:28 +0530

    Update user_main.php

--- user_main.php
+++ user_main.php
@@ -12,37 +12,48 @@
     if (!$conn){
     die("Sorry we failed to connect: ". mysqli_connect_error());
     }
-    echo "Connection was successful";
+    // echo "Connection was successful";
     echo "<br>";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>user's page</title>
 </head>
-<body>
-
+<script>
+function update() {
+  $.get("user_main.php", function(data) {
+    $("#fetch").html(data);
+    window.setTimeout(update, 1000);
+  });
+}
+</script>
+<body onload="update()">
+<div id="fetch">
     <?php
         $i=0;
-        $sql = "SELECT alternate FROM numbers ORDER BY ID DESC LIMIT 1;";
-        if(mysqli_query($conn,$sql)){
+        $sql = "SELECT  alternate FROM numbers ORDER BY ID DESC LIMIT 1;";
+        // $result = mysqli_query($sql);
+        if($result = mysqli_query($conn,$sql)){
             echo "";
-
-            while($row = mysqli_fetch_array($sql)) {
-                $i++;
-                // echo "...";
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)){ 
+                    echo $row["alternate"];
+                    $i++;
+                }
+            // $_POST['usernum'] = $usernum;
             }
-            $_POST['usernum'] = $usernum;
-            // header("location:host.html");
         }
         else{
             echo "Error".mysqli_error($conn);
         }
     ?>
+</div>
 </body>
 </html>
 
